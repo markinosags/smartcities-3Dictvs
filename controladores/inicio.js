b4569eb574f0a5aa49576eeabe157ca257bb0d05
Miguel Fernández <Miguel Fernndez>
2017-03-17 22:27:05 +0100

    Descartada la opción de verDetalles

--- controladores/inicio.js
+++ controladores/inicio.js
@@ -1,21 +1,25 @@
-miAppAngular.controller('inicio', function($scope , configuracionGlobal , $http , $location ){
-    
-    $scope.config = configuracionGlobal;
- 
-      $http.get(configuracionGlobal.api_url1).success(function(respuesta) {
-	    localStorage.setItem('anuncios', JSON.stringify(respuesta));
-	    $scope.anuncios = respuesta.tablons.tablon;
-        $scope.titulo = $scope.anuncios[0].categorias.categoria[0].content;
-        //console.log($scope.anuncios);
-        //console.log($scope.anuncios[1].identificador);
-        //console.log($scope.titulo);
+miAppAngular.controller('inicio', function($scope , configuracionGlobal , $http , $location){
+  
+        $http.get(configuracionGlobal.api_url1).success(function(respuesta) {
+	        localStorage.setItem('anuncios', JSON.stringify(respuesta));
+	        $scope.anuncios = respuesta.tablons.tablon;
+            $scope.titulo = $scope.anuncios[0].categorias.categoria[0].content;
+                //console.log($scope.anuncios);
+                //console.log($scope.anuncios[1].identificador);
+                //console.log($scope.titulo);
+        
         $scope.verDetalles = function(i){
             $location.path("/detalles/"+i);
+            $scope.verDetalles = $scope.anuncios[i];
             $scope.detalle = $scope.anuncios[i];
-            console.log($scope.detalle);
-            console.log($scope.detalle.identificador);
-            console.log($scope.detalle.url);
-            console.log($scope.detalle.send);
-    }
-})        
+            $scope.indice = i;
+            //console.log($scope.detalle);
+            //console.log($scope.detalle.identificador);
+            //console.log($scope.detalle.url);
+            //console.log($scope.detalle.send);
+            console.log($scope);
+        };
+
+    })
+    
 });
